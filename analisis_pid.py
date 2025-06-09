284fb9e668a175c96a8a648ef4aa31ba4a7e7f39
juanpyR <80225854+juanpyR@users.noreply.github.com>
2025-06-09 01:20:48 -0400

    Update analisis_pid.py

--- analisis_pid.py
+++ analisis_pid.py
@@ -17,7 +17,7 @@ generation_config = {
 }
 
 model = genai.GenerativeModel(
-  model_name="gemini-2.0-pro-exp-02-05",
+  model_name="gemini-2.0-pro-exp",
   generation_config=generation_config,
 )
 
